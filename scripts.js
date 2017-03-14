122e61337f8304797bf0b20392c830789bab4a4d
Weronika Musial <jozefinamusial@gmail.com>
2017-03-14 13:03:27 +0100

    poprawione2

--- scripts.js
+++ scripts.js
@@ -3,4 +3,4 @@ var b = prompt('wpisz b');
 var value = (a * a) + (2 * a * b) - (b * b);
 
 console.log('Wynik działania (a * a) + (2 * a * b) - (b * b) wynosi: ' + value);
-console.log(value === 0 ? 'Wynik 0' : (value > 0 ? 'Dodatni' : 'Ujemny' ));
+console.log(value === 0 ? 'Wynik 0' : (value > 0 ? 'Dodatni' : 'Ujemny'));
